9d45bacd683abd7986d4d2fe7a28c7adfea60cf0
Dezhriel <143016251+Dezzie07@users.noreply.github.com>
2024-11-19 16:07:21 +0100

    Update map.py

--- map.py
+++ map.py
@@ -1137,9 +1137,9 @@ def main_storage():
 
     api_pipes, total_distance = get_distance_values()
     if api_pipes:
-    integrate_api_data(pipe_data, api_pipes)
-    st.success("Fetched and integrated pipe data from API successfully!")
-    st.write(f"Total Distance from API: {total_distance} meters")
+        integrate_api_data(pipe_data, api_pipes)
+        st.success("Fetched and integrated pipe data from API successfully!")
+        st.write(f"Total Distance from API: {total_distance} meters")
 
     # Display stored pipes
     st.header("Stored Pipes")
