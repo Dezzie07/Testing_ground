b39772a09b4c48c78e1b8e85f232eddc4b9814f1
Dezhriel <143016251+Dezzie07@users.noreply.github.com>
2024-11-27 07:55:53 +0100

    Update map.py

--- map.py
+++ map.py
@@ -1339,7 +1339,7 @@ def pipe_main():
     
     if landmarks:
         st.subheader("Landmark Data")
-        for name, details in landmarks:
+        for name, details in landmarks.items():
             st.write(f"**{name}**")
             st.write(f"Color: {details['color']}")
             st.write(f"Coordinates: {details['coordinates']}")
