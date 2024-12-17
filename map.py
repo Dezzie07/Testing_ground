95cfa1f3518414507d689f5734d3b76128ad33f7
Dezhriel <143016251+Dezzie07@users.noreply.github.com>
2024-12-17 12:46:06 +0100

    st.empty() in 1476

--- map.py
+++ map.py
@@ -1473,7 +1473,7 @@ def main_storage():
 
     # Save updated storage
     if not save_data(pipe_data):
-        st.error("Failed to save updated storage.")
+        st.empty()
 
     # Display stored pipes and landmarks
     st.header("Stored Pipes and Landmarks")
